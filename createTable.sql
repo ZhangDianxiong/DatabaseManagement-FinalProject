95d60640b14302556a44f0d0265f74c57f4b682e
Lim Cheng Qing <limchengqing@yahoo.com>
2020-03-11 21:07:52 -0700

    Update createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -6,38 +6,32 @@ Country varchar(40),
 City varchar(40),
 Latitude double(16,9),
 Longitude double(16,9),
+UNIQUE(Country, City, Latitude, Longitude),
 PRIMARY KEY(id, City, Latitude, Longitude)
 );
 
 CREATE TABLE Dates (
 ID int NOT NULL AUTO_INCREMENT,
-Years INT,
-Months INT,
-Days INT,
+Years bigint(20),
+Months TINYINT(10),
+Days TINYINT(10),
+UNIQUE(Years, Months, Days),
 PRIMARY KEY(ID)
 );
 
-CREATE TABLE WeatherAUS (
+CREATE TABLE Weather(
 ID int NOT NULL AUTO_INCREMENT,
 dates_ID int NOT NULL, 
-location_ID int NOT NULL, 
-MinTemp double(3,1) NOT NULL, 
-MaxTemp double(3, 1) NOT NULL, 
-RainFall double(3, 1) NOT NULL, 
-Wind_Gust_Direction varchar(4) NOT NULL,
-Wind_Gust_Speed bigint(10) NOT NULL,
-PRIMARY KEY(ID),
-FOREIGN KEY (dates_ID) REFERENCES Dates(ID),
-FOREIGN KEY (location_ID) REFERENCES Location(ID)
-);
-
-CREATE TABLE Mt_Rainier_Weather (
-ID int NOT NULL AUTO_INCREMENT,
-dates_ID int NOT NULL, 
-location_ID int NOT NULL, 
-Average_Temperature double(12,8) NOT NULL,
-Relative_Humidity double(12,8) NOT NULL,
-Wind_Speed_Average double(12,8) NOT NULL,
+location_ID int NOT NULL,
+Temperature_Avg_Fahrenheit bigint(4),
+DewPointAvgF bigint(5),
+Humidity_Avg_Percent bigint(3),
+Sea_Level_Pressure_Avg_Inches double(2,2),
+Visibility_Avg_Miles bigint(5),
+WindAvgMPH bigint(5),
+WindGustMPH bigint(5),
+PrecipitationSumInches double(2,2),
+Weather_event varchar(20),
 PRIMARY KEY(ID),
 FOREIGN KEY (dates_ID) REFERENCES Dates(ID),
 FOREIGN KEY (location_ID) REFERENCES Location(ID)
@@ -63,16 +57,6 @@ FOREIGN KEY (dates_ID) REFERENCES Dates(ID),
 FOREIGN KEY (location_ID) REFERENCES Location(ID)
 );
 
-CREATE TABLE Global_Land_Temperature (
-ID int NOT NULL AUTO_INCREMENT,
-dates_ID int NOT NULL, 
-location_ID int NOT NULL,
-Average_Temperature Double(8,5) NOT NULL,
-Average_Temperature_Uncertainty Double(8,5) NOT NULL,
-PRIMARY KEY(ID),
-FOREIGN KEY (dates_ID) REFERENCES Dates(ID),
-FOREIGN KEY (location_ID) REFERENCES Location(ID)
-);
 
 CREATE TABLE Air_Temperature (
 ID int NOT NULL AUTO_INCREMENT,
@@ -82,6 +66,18 @@ Zonal_Winds Double(1,1),
 Meridional_Winds Double(1,1),
 Air_Temperature Double(3,2),
 Sea_Surface_Temperature Double(3,2),
+PRIMARY KEY(ID),
+FOREIGN KEY (dates_ID) REFERENCES Dates(ID),
+FOREIGN KEY (location_ID) REFERENCES Location(ID)
+);
+
+CREATE TABLE Temperature  (
+ID int NOT NULL AUTO_INCREMENT,
+dates_ID int NOT NULL, 
+location_ID int NOT NULL,
+Minimum_Temperature Decimal(12,8),
+Average_Temperature Decimal(12,8),
+Maxmum_Temperature Decimal(12,8),
 PRIMARY KEY(ID),
 FOREIGN KEY (dates_ID) REFERENCES Dates(ID),
 FOREIGN KEY (location_ID) REFERENCES Location(ID)
@@ -97,7 +93,7 @@ FOREIGN KEY (dates_ID) REFERENCES Dates(ID),
 FOREIGN KEY (location_ID) REFERENCES Location(ID)
 );
 
-CREATE TABLE Austin_Weather(
+CREATE TABLE Weather(
 ID int NOT NULL AUTO_INCREMENT,
 dates_ID int NOT NULL, 
 location_ID int NOT NULL,
@@ -115,12 +111,14 @@ FOREIGN KEY (dates_ID) REFERENCES Dates(ID),
 FOREIGN KEY (location_ID) REFERENCES Location(ID)
 );
 
-CREATE TABLE Global_CO2_Emission (
+CREATE TABLE CO2_Emission (
 ID int NOT NULL AUTO_INCREMENT,
+location_ID int NOT NULL,
 dates_ID int NOT NULL, 
 Average_Emmision Double(8,2),
 Interpolated Double(8,2),
 Trend Double(8,2),
+Unique(location_ID, dates_ID, Average_Emmision, Interpolated, Trend),
 PRIMARY KEY(ID),
 FOREIGN KEY (dates_ID) REFERENCES Dates(ID)
 );
